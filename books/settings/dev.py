119d4730bc69c2f961055949defce52aac7b3efa
nidhin <nidhinp.412@gmail.com>
2019-09-05 14:17:25 +0300

    Updated with postgres and static file

--- books/settings/dev.py
+++ books/settings/dev.py
@@ -8,4 +8,6 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 django_heroku.settings(locals())
