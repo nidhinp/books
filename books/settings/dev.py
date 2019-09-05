d3a52a608733782b29c3456c50ce0519ef382e5e
nidhin <nidhinp.412@gmail.com>
2019-09-05 15:07:57 +0300

    Updated static file settings

--- books/settings/dev.py
+++ books/settings/dev.py
@@ -4,9 +4,8 @@ import django_heroku
 
 from books.settings.base import *
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
